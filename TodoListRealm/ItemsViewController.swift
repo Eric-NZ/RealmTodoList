fb572f8efa6d619bef0d660b08dab296956ae3f9
LomoCode <lomocode@lomobook.local>
2018-09-04 17:34:17 +1200

    completed iOS ToDo Basic Tutorial
    https://docs.realm.io/platform/getting-started-1/ios-quick-start/step-1-my-first-realm-app

---
+++ TodoListRealm/ItemsViewController.swift
@@ -0,0 +1,145 @@
+//
+//  ItemsViewController.swift
+//  TodoListRealm
+//
+//  Created by Eric Ho on 3/09/18.
+//  Copyright © 2018 LomoStudio. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class ItemsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    let realm: Realm            // <- Insert this
+    let items: Results<Item>    // <- Insert this
+    var tableView = UITableView()
+    var notificationToken: NotificationToken?
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ItemCell") ?? UITableViewCell(style: .default, reuseIdentifier: "ItemCell")
+        
+        let item = items[indexPath.row]
+        
+        cell.textLabel?.text = item.body
+        cell.selectionStyle = .none
+        cell.accessoryType = item.isDone ? .checkmark : .none
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let item = items[indexPath.row]
+        try! realm.write {
+            item.isDone = !item.isDone
+        }
+    }
+    
+    /* Implement the Wipe to Delete functionality
+     */
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        guard editingStyle == .delete else {return}
+        
+        try! realm.write {
+            realm.delete(items[indexPath.row])
+        }
+    }
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+
+        // Essential: Init self.realm.
+        let syncConfig = SyncConfiguration(user: SyncUser.current!, realmURL: Constants.REALM_URL)
+        self.realm = try! Realm(configuration: Realm.Configuration(syncConfiguration: syncConfig, objectTypes: [Item.self]))
+        // Essential: Init self.items.
+        self.items = realm.objects(Item.self).sorted(byKeyPath: "timestamp", ascending: false)
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        notificationToken?.invalidate()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // right button
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Logout", style: .plain, target: self, action: #selector(rightBarButtonDidClick))
+        // left button
+        navigationItem.leftBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(addDidClicked))
+        
+        // set up tableView as a subview
+        self.title = "To Do Item"
+        tableView.delegate = self
+        tableView.dataSource = self
+        self.view.addSubview(tableView)
+        tableView.frame = self.view.frame
+        
+        // Add Notification Handle
+        addNotificationHandle()
+        
+    }
+    
+    func addNotificationHandle() {
+        notificationToken = items.observe { [weak self] (changes) in
+            guard let tableView = self?.tableView else { return }
+            switch changes {
+            case .initial:
+                // Results are now populated and can be accessed without blocking the UI
+                tableView.reloadData()
+            case .update(_, let deletions, let insertions, let modifications):
+                // Query results have changed, so apply them to the UITableView
+                tableView.beginUpdates()
+                tableView.insertRows(at: insertions.map({ IndexPath(row: $0, section: 0) }),
+                                     with: .automatic)
+                tableView.deleteRows(at: deletions.map({ IndexPath(row: $0, section: 0)}),
+                                     with: .automatic)
+                tableView.reloadRows(at: modifications.map({ IndexPath(row: $0, section: 0) }),
+                                     with: .automatic)
+                tableView.endUpdates()
+            case .error(let error):
+                // An error occurred while opening the Realm file on the background worker thread
+                fatalError("\(error)")
+            }
+        }
+    }
+    
+    @objc func addDidClicked() {
+        let alertController = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "Save", style: .default, handler: {
+            alert -> Void in
+            let textField = alertController.textFields![0] as UITextField
+            let item = Item()
+            item.body = textField.text ?? ""
+            try! self.realm.write {
+                self.realm.add(item)
+            }
+            // do something with textField
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alertController.addTextField(configurationHandler: {(textField : UITextField!) -> Void in
+            textField.placeholder = "New Item Text"
+        })
+        self.present(alertController, animated: true, completion: nil)
+
+    }
+    
+    @objc func rightBarButtonDidClick() {
+        let alertController = UIAlertController(title: "Logout", message: "", preferredStyle: .alert)
+        alertController.addAction(UIAlertAction(title: "Yes, Logout", style: .destructive, handler: {
+            alert -> Void in
+            SyncUser.current?.logOut()
+            self.navigationController?.setViewControllers([WelcomeViewController()], animated: true)
+        }))
+        alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        self.present(alertController, animated: true, completion: nil)
+    }
+}
---
+++ TodoListRealm/WelcomeViewController.swift
@@ -0,0 +1,51 @@
+//
+//  WelcomeViewController.swift
+//  TodoListRealm
+//
+//  Created by Eric Ho on 3/09/18.
+//  Copyright © 2018 LomoStudio. All rights reserved.
+//
+
+import UIKit
+import RealmSwift
+
+class WelcomeViewController: UIViewController {
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        title = "Welcome"
+        
+        if let _ = SyncUser.current {
+            // We have already logged in here!
+            self.navigationController?.pushViewController(ItemsViewController(), animated: true)
+        } else {
+            let alertController = UIAlertController(title: "Login to Realm Cloud", message: "Supply a nice nickname!", preferredStyle: .alert)
+            
+            alertController.addAction(UIAlertAction(title: "Login", style: .default, handler: { [unowned self]
+                alert -> Void in
+                let textField = alertController.textFields![0] as UITextField
+                let creds = SyncCredentials.nickname(textField.text!, isAdmin: true)
+                
+                SyncUser.logIn(with: creds, server: Constants.AUTH_URL, onCompletion: { [weak self](user, err) in
+                    if let _ = user {
+                        self?.navigationController?.pushViewController(ItemsViewController(), animated: true)
+                    } else if let error = err {
+                        fatalError(error.localizedDescription)
+                    }
+                })
+            }))
+            alertController.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+            alertController.addTextField(configurationHandler: {(textField : UITextField!) -> Void in
+                textField.placeholder = "A Name for your user"
+            })
+            self.present(alertController, animated: true, completion: nil)
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
